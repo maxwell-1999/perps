d856294f4bb6f01b41fceb35a50aa654158c8bd8
Alan <ajohnson88@gmail.com>
2023-05-01 15:21:26 -0400

    Add link switcher for nav

---
+++ src/components/LinkSwitcher/__tests__/LinkSwitcher.spec.tsx
@@ -0,0 +1,50 @@
+import { render as rtlRender, screen } from "@testing-library/react";
+import LinkSwitcher from "../index"; // Update this import path
+
+jest.mock("next/dist/client/router", () => ({
+  __esModule: true,
+  useRouter: () => ({
+    query: {},
+    pathname: "/",
+    asPath: "/",
+    events: {
+      emit: jest.fn(),
+      on: jest.fn(),
+      off: jest.fn(),
+    },
+    push: jest.fn(() => Promise.resolve(true)),
+    prefetch: jest.fn(() => Promise.resolve(true)),
+    replace: jest.fn(() => Promise.resolve(true)),
+  }),
+}));
+
+function render(ui: JSX.Element, { ...renderOptions } = {}) {
+  return rtlRender(ui, { ...renderOptions });
+}
+
+describe("LinkSwitcher", () => {
+  const testLinks = [
+    { href: "/", label: "Home" },
+    { href: "/page1", label: "Page 1" },
+    { href: "/page2", label: "Page 2" },
+  ];
+
+  it("renders without crashing", () => {
+    render(<LinkSwitcher links={testLinks} />);
+  });
+
+  it("renders the correct number of links", () => {
+    render(<LinkSwitcher links={testLinks} />);
+
+    const links = screen.getAllByRole("link");
+    expect(links).toHaveLength(testLinks.length);
+  });
+
+  it("renders the correct link text", () => {
+    render(<LinkSwitcher links={testLinks} />);
+
+    testLinks.forEach(({ label }) => {
+      expect(screen.getByText(label)).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/components/LinkSwitcher/index.tsx
@@ -0,0 +1,36 @@
+import { Box } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Button } from "../design-system/Button";
+
+interface NavLinkProps {
+  href: string;
+  label: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, label }) => {
+  const { pathname } = useRouter();
+  const isActive = pathname === href;
+
+  return (
+    <Link href={href} passHref>
+      <Button label={label} variant={isActive ? "transparent" : "ghost"} mr={1} />
+    </Link>
+  );
+};
+
+interface LinkSwitcherProps {
+  links: NavLinkProps[];
+}
+
+const LinkSwitcher: React.FC<LinkSwitcherProps> = ({ links }) => {
+  return (
+    <Box display="flex" p={4}>
+      {links.map((link) => (
+        <NavLink key={link.href} href={link.href} label={link.label} />
+      ))}
+    </Box>
+  );
+};
+
+export default LinkSwitcher;
