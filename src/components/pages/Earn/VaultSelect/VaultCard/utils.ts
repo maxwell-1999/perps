3ceb64dc86756204d28dcbffb584967b631f0877
Alan <ajohnson88@gmail.com>
2023-06-14 16:20:04 -0400

    pr edit

--- src/components/pages/Earn/VaultSelect/VaultCard/utils.ts
+++ src/components/pages/Earn/VaultSelect/VaultCard/utils.ts
@@ -1,6 +1,5 @@
 import { Big18Math } from '@/utils/big18Utils'
 
 export function formatValueForProgressBar(value: bigint, total: bigint) {
-  const outOf100 = Big18Math.mul(Big18Math.div(value, total), Big18Math.fromFloatString('100'))
-  return Math.floor(Big18Math.fixedFrom(outOf100).toUnsafeFloat())
+  return Math.floor(Big18Math.divFixed(value, total).toUnsafeFloat() * 100)
 }
