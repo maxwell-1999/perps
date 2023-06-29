1be2fec2699cd5ff3462cc87d6631385aa8f740b
Alan <ajohnson88@gmail.com>
2023-06-29 10:45:59 -0400

    link to v1 for ethereum vaults

--- src/components/pages/Earn/VaultDetail/hooks.tsx
+++ src/components/pages/Earn/VaultDetail/hooks.tsx
@@ -69,6 +69,14 @@ export const useVaultDetailCopy = () => {
     withdrawPending: intl.formatMessage({ defaultMessage: 'Withdraw pending' }),
     positionSettled: intl.formatMessage({ defaultMessage: 'Position settled' }),
     yourPositionHasSettled: intl.formatMessage({ defaultMessage: 'Your position has settled' }),
+    ethereumUnavailable: intl.formatMessage({
+      defaultMessage:
+        'Perennial Vaults are not available on Ethereum L1. A new Pro LP maker experience is coming soon to all networks.',
+    }),
+    ethereumUnavailableLink: intl.formatMessage({
+      defaultMessage: 'If you have existing LP positions you can manage them at ',
+    }),
+    linkText: intl.formatMessage({ defaultMessage: 'https://v1.app.perennial.finance.' }),
   }
 }
 
