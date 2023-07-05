e2cbfcc9f35a2a65a0216ebe0b9ef8e90d7b352e
arjun-io <2940142+arjun-io@users.noreply.github.com>
2023-07-05 12:09:31 -0400

    Update Fee APR (#147)

--- src/constants/vaults.ts
+++ src/constants/vaults.ts
@@ -46,8 +46,8 @@ export const FeeApr: { [chainId: number]: { [key in VaultSymbol]?: bigint } } = 
     [VaultSymbol.PVB]: Big18Math.fromFloatString('0.1206'),
   },
   [arbitrum.id]: {
-    [VaultSymbol.PVA]: Big18Math.fromFloatString('0.3619'),
-    [VaultSymbol.PVB]: Big18Math.fromFloatString('0.9623'),
+    [VaultSymbol.PVA]: Big18Math.fromFloatString('0.3625'),
+    [VaultSymbol.PVB]: Big18Math.fromFloatString('0.9349'),
   },
   [baseGoerli.id]: {
     [VaultSymbol.PVA]: Big18Math.fromFloatString('0.1391'),
