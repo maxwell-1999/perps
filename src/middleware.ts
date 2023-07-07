f1648a2eee4096412d1f80f71454e7c6d0bb7b07
arjun-io <2940142+arjun-io@users.noreply.github.com>
2023-07-07 13:24:54 -0400

    don't run middleware on tradingview (#159)

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - tradingview (tradingview widget files)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|tradingview).*)',
   ],
 }
