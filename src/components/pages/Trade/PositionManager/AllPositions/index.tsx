f35b064ad064e55d83f312988f6735744fbe0988
Alan <alan@equilibria.xyz>
2023-07-30 19:49:40 -0400

    maker market select from all positions

--- src/components/pages/Trade/PositionManager/AllPositions/index.tsx
+++ src/components/pages/Trade/PositionManager/AllPositions/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { LoadingScreen } from '@/components/shared/components'
+import { SupportedMakerMarket } from '@/constants/assets'
 import { OrderDirection } from '@/constants/markets'
 import { PositionsTab, useMarketContext } from '@/contexts/marketContext'
 import { useAddress } from '@/hooks/network'
@@ -13,14 +14,29 @@ import { useOpenPositionTableData, usePositionManagerCopy } from '../hooks'
 function AllPositions() {
   const { noCurrentPositions, connectWalletPositions } = usePositionManagerCopy()
   const { address } = useAddress()
-  const { setSelectedMarket, setOrderDirection, setActivePositionTab, selectedMarket, orderDirection } =
-    useMarketContext()
+  const {
+    setSelectedMarket,
+    setOrderDirection,
+    setActivePositionTab,
+    selectedMarket,
+    orderDirection,
+    isMaker,
+    selectedMakerMarket,
+    setSelectedMakerMarket,
+  } = useMarketContext()
   const { positions: tableData, status } = useOpenPositionTableData()
   const handleRowClick = (row: PositionTableData) => {
     setActivePositionTab(PositionsTab.current)
-    if (row.asset === selectedMarket && row.details.direction === orderDirection) return
-    setSelectedMarket(row.asset)
-    setOrderDirection(row.details.direction ?? OrderDirection.Long)
+    if (!isMaker) {
+      if (row.asset === selectedMarket && row.details.direction === orderDirection) return
+      setSelectedMarket(row.asset)
+      setOrderDirection(row.details.direction ?? OrderDirection.Long)
+    } else {
+      const { asset, direction } = row.details
+      const makerMarket = `${asset}-${direction}` as SupportedMakerMarket
+      if (makerMarket === selectedMakerMarket) return
+      setSelectedMakerMarket(makerMarket)
+    }
   }
 
   if (!address) return <TableEmptyScreen message={connectWalletPositions} />
