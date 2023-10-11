bd6c7329a87144d0eb4cf8e476f32917d85afc9e
Alan <alan@equilibria.xyz>
2023-10-11 15:49:49 -0400

    manually retrigger leverage validation on maxLeverage change

--- src/components/pages/Trade/TradeForm/components/TradeForm.tsx
+++ src/components/pages/Trade/TradeForm/components/TradeForm.tsx
@@ -131,6 +131,7 @@ function TradeForm(props: TradeFormProps) {
     watch,
     setValue,
     reset,
+    trigger,
     formState: { dirtyFields, errors },
   } = useForm({
     defaultValues: initialFormState,
@@ -146,6 +147,11 @@ function TradeForm(props: TradeFormProps) {
     () => calcMaxLeverage({ margin, minMargin, collateral: Big6Math.fromFloatString(collateral) }),
     [margin, minMargin, collateral],
   )
+
+  useEffect(() => {
+    // Manually trigger leverage validation on maxLeverage change
+    trigger(FormNames.leverage)
+  }, [maxLeverage, trigger])
 
   const resetInputs = useCallback(() => {
     reset({ ...initialFormState })
